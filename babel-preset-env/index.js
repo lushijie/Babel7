0b9354fe8a0437d33a448977396688252065026a
lushijie <lushijie03@meituan.com>
2019-03-12 18:05:22 +0800

    update

--- babel-preset-env/index.js
+++ babel-preset-env/index.js
@@ -1,6 +1,6 @@
 import '@babel/polyfill';
 // import _ from 'lodash'; unused 引入也会增加包的体积
-import './index2'; // entry 方式 如果 index2 也包含@babel/polyfill，体积会变大（引用）
+// import './index2'; // entry 方式 如果 index2 也包含@babel/polyfill，体积会变大（引用）
 
 'footbar'.includes('foo');
 class Person {};
